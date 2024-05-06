b1b4caefe1b0f68aa04eb9241f4464ab7fd32480
John L <johnny@synergetx.io>
2024-05-06 12:45:37 -0700

    Add store item selection

--- components/StoreItemsList.tsx
+++ components/StoreItemsList.tsx
@@ -1,4 +1,4 @@
-import { Salad } from '@tamagui/lucide-icons';
+import { Check, Salad } from '@tamagui/lucide-icons';
 import { useField, useFormikContext } from 'formik';
 import Fuse, { IFuseOptions } from 'fuse.js';
 import { memo, useMemo } from 'react';
@@ -10,42 +10,27 @@ import { ListItemWithData } from '@/types/list';
 
 const fuseOptions: IFuseOptions<ListItemWithData> = {
     minMatchCharLength: 2,
-    threshold: 0.4,
+    threshold: 0.1,
     keys: [{ name: 'item_name', weight: 2.5 }],
 };
 
 const StoreItemsList = () => {
     const { allStoreItemsWithCost } = useListItemsProviderContext();
     const [{ value: itemName }, ,] = useField<string>('item_name');
-    const { setValues, handleSubmit } = useFormikContext<InitialListItemFormValue>();
     const fuseList = useMemo(
         () => new Fuse(allStoreItemsWithCost, fuseOptions),
         [allStoreItemsWithCost],
     );
+
+    console.log({ fuseList });
     return itemName ? (
         <XStack>
             <ScrollView horizontal flexDirection="row">
                 <XStack gap="$2">
-                    {fuseList.search(itemName).map(({ item }) => (
-                        <ListItem
-                            key={item.created_at}
-                            icon={Salad}
-                            title={item.item_name}
-                            subTitle={`$${item.price}`}
-                            flex={1}
-                            paddingVertical="$2"
-                            paddingHorizontal="$3"
-                            backgroundColor="$green5"
-                            borderRadius="$3"
-                            onPress={() => {
-                                setValues({
-                                    ...item,
-                                    quantity: item.quantity?.toString() ?? '0',
-                                    price: item.price?.toString() ?? '0',
-                                }).then(() => handleSubmit());
-                            }}
-                        />
-                    ))}
+                    {fuseList.search(itemName).map(({ item }) => {
+                        console.log({ item });
+                        return <StoreItem item={item} />;
+                    })}
                 </XStack>
             </ScrollView>
         </XStack>
@@ -53,3 +38,30 @@ const StoreItemsList = () => {
 };
 
 export default memo(StoreItemsList);
+
+const StoreItem = ({ item }: { item: ListItemWithData }) => {
+    const { setValues, handleSubmit, values } = useFormikContext<InitialListItemFormValue>();
+
+    const isChosen = useMemo(() => values.item_id === item.item_id, [item.item_id, values.item_id]);
+
+    return (
+        <ListItem
+            key={item.created_at}
+            icon={isChosen ? Check : Salad}
+            backgroundColor={isChosen ? '$green6' : '$green4'}
+            title={item.item_name}
+            subTitle={`$${item.price}`}
+            flex={1}
+            paddingVertical="$2"
+            paddingHorizontal="$3"
+            borderRadius="$3"
+            onPress={() => {
+                setValues({
+                    ...item,
+                    quantity: item.quantity?.toString() ?? '0',
+                    price: item.price?.toString() ?? '0',
+                }).then(() => handleSubmit());
+            }}
+        />
+    );
+};
